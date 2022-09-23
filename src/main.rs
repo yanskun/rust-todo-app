37e77056963f2b2aedeed95080784e3cad0f35f9
yanskun <43776161+yanskun@users.noreply.github.com>
2022-09-23 23:22:26 +0900

    feat: add todo struct impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,36 @@
+use std::collections::HashMap;
+
+struct Todo {
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
+}
+
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    println!("{:?}, {:?}", action, item);
+    let mut todo = Todo {
+        map: HashMap::new(),
+    };
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    }
 }
